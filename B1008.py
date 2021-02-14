bc5126481d433aa0e5d43ac2dab8a551004270df
BrielleZ <40680659+BrielleZ@users.noreply.github.com>
2021-02-14 16:32:59 +0800

    Delete B1008.py

--- B1008.py
+++
@@ -1,9 +0,0 @@
-# B1008
-n, mov = map(int, input().split())
-mov = mov % n
-num = input().split()
-if mov == 0:
-    num_new = num
-else:
-    num_new = num[-mov:] + num[:n - mov]
-print(' '.join(num_new))
